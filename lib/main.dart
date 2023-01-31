2216bf7f1d63373f879895084fada64e2563ddda
oli-mac <adumulugeta21@gmail.com>
2023-01-31 11:49:43 +0300

    search page initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:enebla_user_app/search/search.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,10 +14,32 @@ class EneblaUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 17, 66, 204),
-          title: Text(title)),
-    ));
+      home: EneblaHome(),
+    );
+  }
+}
+
+class EneblaHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           backgroundColor: Color.fromARGB(255, 17, 66, 204),
+           title: Text('Enebla'),
+           actions: [
+             IconButton(onPressed: (){
+               Navigator.push(
+                   context,
+                   MaterialPageRoute(
+                       builder: (context) =>
+                           Search()));
+              }, icon: const Icon(Icons.search))
+           ],
+         ),
+
+
+       )
+   );
   }
 }
