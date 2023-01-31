09e3e80b2d7c85d2b49462702c7c5d23055a44e6
oli-mac <adumulugeta21@gmail.com>
2023-02-01 02:18:03 +0300

    order page initialized:preview order

--- lib/screens/order/orderPreview.dart
+++ lib/screens/order/orderPreview.dart
@@ -1,4 +1,7 @@
+import 'package:enebla_user_app/enebla_home.dart';
 import 'package:flutter/material.dart';
+import 'package:enebla_user_app/style.dart' as style;
+
 class OrderPreview extends StatelessWidget {
   const OrderPreview({Key? key}) : super(key: key);
 
@@ -26,70 +29,174 @@ class OrderPreview extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: 50,
-                itemBuilder: (context, index){
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 130,
-                      margin: EdgeInsets.only(bottom: 15),
-                      decoration: const BoxDecoration(
-                      // color: Colors.red,
-                      borderRadius: BorderRadius.all(Radius.circular(15))
-                  ),
-                  child:Row(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: 50,
+                  itemBuilder: (context, index){
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 80,
+                        margin: EdgeInsets.only(bottom: 15),
+                        decoration: const BoxDecoration(
+                        // color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(15))
+                    ),
+                      child:Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children:  <Widget>[
+                          Text(
+                            '1',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Text(
+                            'Maheberawi',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Text(
+                            '140ETB',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                              color: style.Style.primaryColor,
+                            ),
+                          ),
+                        ],
+                      )
+                    );
+                  }),
+              ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height/2.4,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment:CrossAxisAlignment.start,
+                mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: const <Widget>[
                       Text(
-                        'Ethiopia',
+                        'Subtotal',
                         style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.black54,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
                         ),
                       ),
                       Text(
-                        '140ETB',
+                        '145ETB',
+                        textAlign: TextAlign.end,
                         style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.green,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
                         ),
                       ),
                     ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const <Widget>[
+                      Text(
+                        'VAT',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
+                        ),
+                      ),
+                      Text(
+                        '15%',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const <Widget>[
+                      Text(
+                        'Total Price',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
+                        ),
+                      ),
+                      Text(
+                        '189Etb',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment:CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: () { EneblaHome(); },
+                        child:  Text(
+                          'Add more items',
+                          style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          color: style.Style.primaryColor,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () { EneblaHome(); },
+                          icon: Icon( Icons.chevron_right),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    margin: EdgeInsets.only(left: 15,right: 15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: style.Style.primaryColor,
+                        ),
+                          onPressed: (){},
+                          child: const Text(
+                            'Continue To Order',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white
+                            ),
+                          ),
+                      ),
+                    ),
                   )
-                  );
-                }),
+                ],
+              ),
+            )
           ),
-          // Container(
-          //   child: Column(
-          //     children: [
-          //       // Row(
-          //       //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //       //   children: const <Widget>[
-          //       //     Text(
-          //       //       'UPCOMING ORDERS',
-          //       //       style: TextStyle(
-          //       //           fontSize: 18,
-          //       //           fontWeight: FontWeight.w800,
-          //       //           color: Colors.black54
-          //       //       ),
-          //       //     ),
-          //       //     Text(
-          //       //       'View History',
-          //       //       textAlign: TextAlign.end,
-          //       //       style: TextStyle(
-          //       //           fontSize: 14,
-          //       //           fontWeight: FontWeight.w800,
-          //       //           color: Colors.black
-          //       //       ),
-          //       //     ),
-          //       //   ],
-          //       // ),
-          //     ],
-          //   )
-          // ),
         ],
       ),
     );
