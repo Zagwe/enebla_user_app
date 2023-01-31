442f0dd52967afa74c5aa6ba5121268d8e4d033e
oli-mac <adumulugeta21@gmail.com>
2023-02-01 00:15:23 +0300

    order page initialized:list orders

--- lib/screens/order/order.dart
+++ lib/screens/order/order.dart
@@ -26,16 +26,123 @@ class Order extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text('first'),
-              Text('Second')
-              
-            ],
-          )
-        ]
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                Text(
+                  'UPCOMING ORDERS',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.black54,
+                      wordSpacing: 5
+                  ),
+                ),
+                Text(
+                  'View History',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.black,
+                      wordSpacing: 5
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                   width: MediaQuery.of(context).size.width,
+                    height: 130,
+                    margin: EdgeInsets.only(bottom: 15),
+                    decoration: const BoxDecoration(
+                      // color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(15))
+                    ),
+                    child: Row(
+                      children: [
+                        const ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          child: Image(
+                              image: AssetImage(
+                                  "lib/assets/home.jpg"),
+                            width: 120,
+                            height: 180,
+                            fit: BoxFit.cover
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                            crossAxisAlignment:CrossAxisAlignment.start,
+                            mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                                const Text(
+                                  'McDonald',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w800,
+                                      color: Colors.black,
+                                      wordSpacing: 5
+                                  ),
+                                ),
+                                  const  Flexible(
+                                      child: Text(
+                                        'Shortbread, chocolate turtle cookies, and red velvet.',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w800,
+                                            color: Colors.black54,
+                                            // wordSpacing: 5
+                                        ),
+                                      ),
+
+                                  ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: const <Widget>[
+                                    Text(
+                                      'Ethiopia',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w800,
+                                          color: Colors.black54,
+                                      ),
+                                    ),
+                                    Text(
+                                      '140ETB',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w800,
+                                          color: Colors.green,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                ,),
+            )
+          ],
+        ),
       )
     );
   }
