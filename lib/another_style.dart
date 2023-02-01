ccb365ff79106021ebde7a2af11cf10bee7ef303
tarik350 <80385670+tarik350@users.noreply.github.com>
2023-02-01 21:51:51 +0300

    working on resturant page menu section

---
+++ lib/another_style.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:enebla_user_app/style.dart' as style;
+
+class Style {
+  static var appBarTheme = const AppBarTheme(
+      titleSpacing: 0,
+      elevation: 0,
+      backgroundColor: Colors.white,
+      toolbarHeight: 80);
+  static var textTheme = const TextTheme(
+      headlineLarge: TextStyle(
+          fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+      headlineSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.bold));
+
+  static var tabbarTheme = TabBarTheme(
+      indicator: UnderlineTabIndicator(
+          // insets: EdgeInsets.only(right: 60.0, left: 16, top: 50),
+          borderSide: BorderSide(width: 5, color: style.Style.primaryColor)),
+      indicatorSize: TabBarIndicatorSize.label,
+      labelColor: Colors.black,
+      unselectedLabelColor: Colors.black26,
+      unselectedLabelStyle: TextStyle(fontSize: 20),
+      labelStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+
+  static var themeData = ThemeData(
+      textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+        overlayColor:
+            MaterialStateColor.resolveWith((states) => Colors.transparent),
+      )),
+      textTheme: Style.textTheme,
+      appBarTheme: Style.appBarTheme,
+      tabBarTheme: Style.tabbarTheme,
+      primaryColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white);
+}
