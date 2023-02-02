381c33fcaa1c2bf3edf7ca4a8aef883a272cfe8b
mik <michaelbelachew09@gmail.com>
2023-02-02 12:04:03 +0300

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:enebla_user_app/theme/another_style.dart' as style;
 
+//added some comment
+
 import 'enebla_home.dart';
 
 Future<void> main() async {
