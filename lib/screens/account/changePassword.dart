941b15b7653cef71f2c2d0d7e80ad43399fdc230
mik <michaelbelachew09@gmail.com>
2023-02-09 22:12:59 +0300

    working on change password

--- lib/screens/account/changePassword.dart
+++ lib/screens/account/changePassword.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:enebla_user_app/auth/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -15,20 +16,16 @@ class BackupSettings extends StatefulWidget {
 class _BackupSettingsState extends State<BackupSettings> {
   final _newpasswordController = TextEditingController();
   final _repeatpasswordController = TextEditingController();
-  var _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
-  void _changePassword(String currentPassword, String newPassword) async {
-    final user = await FirebaseAuth.instance.currentUser;
-    final cred = EmailAuthProvider.credential(
-        email: user.email, password: currentPassword);
+  _changePassword(String yourPassword) async {
+    User? user = FirebaseAuth.instance.currentUser;
 
-    user.reauthenticateWithCredential(cred).then((value) {
-      user.updatePassword(newPassword).then((_) {
-        //Success, do something
-      }).catchError((error) {
-        //Error, show something
-      });
-    }).catchError((err) {});
+    user!.updatePassword(yourPassword).then((_) {
+      print('update done');
+    }).catchError((error) {
+      print("Error " + error.toString());
+    });
   }
 
   bool checkCurrentPasswordValid = true;
@@ -36,18 +33,19 @@ class _BackupSettingsState extends State<BackupSettings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           ('backup_text'),
         ),
         elevation: 0,
-        brightness: Brightness.light,
         backgroundColor: Colors.black,
         leading: IconButton(
           onPressed: () {
-            Navigator.pop(context,
-                MaterialPageRoute(builder: (context) => AccountSetting()));
+            Navigator.pop(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const AccountSetting()));
           },
-          icon: Icon(
+          icon: const Icon(
             Icons.arrow_back_ios,
             size: 20,
             color: Colors.black,
@@ -56,33 +54,33 @@ class _BackupSettingsState extends State<BackupSettings> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             key: _formKey,
             children: <Widget>[
-              Container(
-                child: TextFormField(
-                    validator: (input) {
-                      if (input!.length < 8)
-                        return 'Please Provide Minimum 8 Character';
-                    },
-                    decoration: InputDecoration(
-                      labelText: "new_password",
-                      labelStyle: TextStyle(color: Colors.grey),
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.black,
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
+              TextFormField(
+                  validator: (input) {
+                    if (input!.length < 8) {
+                      return 'Please Provide Minimum 8 Character';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: "new_password",
+                    labelStyle: TextStyle(color: Colors.grey),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.black,
                     ),
-                    obscureText: true,
-                    controller: _newpasswordController),
-              ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                  obscureText: true,
+                  controller: _newpasswordController),
               Container(
                 child: TextFormField(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'repeat_password',
                     labelStyle: TextStyle(color: Colors.grey),
                     prefixIcon: Icon(
@@ -102,17 +100,21 @@ class _BackupSettingsState extends State<BackupSettings> {
                   },
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
                 // padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
-                onPressed: () {},
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _changePassword(_newpasswordController.text);
+                  }
+                },
 
-                child: Text(('save_button'),
+                style: ElevatedButton.styleFrom(),
+                child: const Text(('save_button'),
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold)),
-                style: ElevatedButton.styleFrom(),
               )
             ],
           ),
